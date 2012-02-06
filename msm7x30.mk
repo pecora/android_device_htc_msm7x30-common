f5378adbd9365001a47194b8b1924a00ba3bb883
Twisted <twistedumbrella@gmail.com>
2012-02-06 11:15:05 -0500

    Organize Vendor Default - Make More Packages Optional

--- msm7x30.mk
+++ msm7x30.mk
@@ -84,67 +84,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
 
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-    Basic \
-    HoloSpiralWallpaper \
-    MagicSmokeWallpapers \
-    NoiseField \
-    Galaxy4 \
-    PhaseBeam \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    VisualizationWallpapers
-
-# Filesystem management tools
-PRODUCT_PACKAGES += \
-    make_ext4fs \
-    setup_fs
-
-# Applications
-PRODUCT_PACKAGES += \
-    Development \
-    FileManager \
-    Mms \
-    Stk \
-    VoiceDialer \
-    SpareParts \
-    Torch \
-    Superuser \
-    su
-
 # Prebuilt Camera
 PRODUCT_COPY_FILES += \
     device/htc/msm7x30-common/system/app/Camera.apk:system/app/Camera.apk
-
-# CyanogenMod Packages
-PRODUCT_PACKAGES += \
-    DSPManager \
-    libcyanogen-dsp \
-    audio_effects.conf
-
-# Google Applications
-PRODUCT_COPY_FILES += \
-    vendor/twisted/google/app/GenieWidget.apk:system/app/GenieWidget.apk \
-    vendor/twisted/google/app/Street.apk:system/app/Street.apk \
-    vendor/twisted/google/app/Thinkfree.apk:system/app/Thinkfree.apk \
-    vendor/twisted/google/app/Videos.apk:system/app/Videos.apk \
-    vendor/twisted/google/app/VoiceSearch.apk:system/app/VoiceSearch.apk
-
-# Google Libraries
-PRODUCT_COPY_FILES += \
-    vendor/twisted/google/lib/libfacelock_jni.so:system/lib/libfacelock_jni.so \
-    vendor/twisted/google/lib/libfilterpack_facedetect.so:system/lib/libfilterpack_facedetect.so \
-    vendor/twisted/google/lib/libflint_engine_jni_api.so:system/lib/libflint_engine_jni_api.so
-
-# Google Vendor
-PRODUCT_COPY_FILES += \
-    vendor/twisted/google/vendor/firmware/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so \
-    vendor/twisted/google/vendor/pittpatt:system/vendor/pittpatt
-
-# Google Customization
-PRODUCT_COPY_FILES += \
-    vendor/twisted/twisted/media/bootanimation.zip:system/media/bootanimation.zip
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
